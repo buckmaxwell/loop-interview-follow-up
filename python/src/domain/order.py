6bee55081ab4ff4095cfc5abc17c9c7c4899abe8
Max Buck <maxbuckdeveloper@gmail.com>
2025-04-24 17:32:22 -0400

    approve and reject working on order

--- python/src/domain/order.py
+++ python/src/domain/order.py
@@ -3,12 +3,40 @@ from dataclasses import dataclass, field
 from .order_item import OrderItem
 from .order_status import OrderStatus
 
+from python.src.use_case.exceptions import (
+    ShippedOrdersCannotBeChangedException,
+    RejectedOrderCannotBeApprovedException,
+    ApprovedOrderCannotBeRejectedException,
+)
+
 
 @dataclass
 class Order:
     id: int = 0
     total: float = 0
-    currency: str = 'EUR'
+    currency: str = "EUR"
     items: list[OrderItem] = field(default_factory=list)
     tax: float = 0
     status: OrderStatus = OrderStatus.CREATED
+
+    def approve(self):
+        self._raise_if_order_shipped()
+        self._raise_if_order_was_rejected()
+        self.status = OrderStatus.APPROVED
+
+    def reject(self):
+        self._raise_if_order_shipped()
+        self._raise_if_order_was_approved()
+        self.status = OrderStatus.REJECTED
+
+    def _raise_if_order_shipped(self):
+        if self.status == OrderStatus.SHIPPED:
+            raise ShippedOrdersCannotBeChangedException()
+
+    def _raise_if_order_was_rejected(self):
+        if self.status == OrderStatus.REJECTED:
+            raise RejectedOrderCannotBeApprovedException()
+
+    def _raise_if_order_was_approved(self):
+        if self.status == OrderStatus.APPROVED:
+            raise ApprovedOrderCannotBeRejectedException()
--- python/src/use_case/order_approval_use_case.py
+++ python/src/use_case/order_approval_use_case.py
@@ -1,7 +1,4 @@
-from ..domain.order_status import OrderStatus
-from ..repository.order_repository import OrderRepository
-from .exceptions import ShippedOrdersCannotBeChangedException, \
-    RejectedOrderCannotBeApprovedException, ApprovedOrderCannotBeRejectedException
+from python.src.repository.order_repository import OrderRepository
 from .order_approval_request import OrderApprovalRequest
 
 
@@ -12,14 +9,9 @@ class OrderApprovalUseCase:
     def run(self, request: OrderApprovalRequest) -> None:
         order = self._order_repository.get_by_id(request.order_id)
 
-        if order.status == OrderStatus.SHIPPED:
-            raise ShippedOrdersCannotBeChangedException()
+        if request.approved:
+            order.approve()
+        else:
+            order.reject()
 
-        if request.approved and order.status == OrderStatus.REJECTED:
-            raise RejectedOrderCannotBeApprovedException()
-
-        if not request.approved and order.status == OrderStatus.APPROVED:
-            raise ApprovedOrderCannotBeRejectedException()
-
-        order.status = OrderStatus.APPROVED if request.approved else OrderStatus.REJECTED
         self._order_repository.save(order)
